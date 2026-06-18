b8635448887ca7be1817169b3959481a5d80e0e5
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2026-06-18 09:41:51 +0200

    fix: add windows sdk fallback paths

--- .github/scripts/setup-msvc.cmd
+++ .github/scripts/setup-msvc.cmd
@@ -3,6 +3,8 @@
 set "VSINSTALLDIR="
 set "NMAKE="
 set "CLDIR="
+set "UCRT_INCLUDE="
+set "UCRT_LIB="
 
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do set "VSINSTALLDIR=%%i"
 if not defined VSINSTALLDIR exit /b 1
@@ -24,6 +26,11 @@ set "PATH=%CLDIR%;%PATH%"
 
 if defined UniversalCRTSdkDir if defined UCRTVersion if exist "%UniversalCRTSdkDir%Include\%UCRTVersion%ucrt\crtdefs.h" set "INCLUDE=%UniversalCRTSdkDir%Include\%UCRTVersion%ucrt;%INCLUDE%"
 if defined UniversalCRTSdkDir if defined UCRTVersion if exist "%UniversalCRTSdkDir%Lib\%UCRTVersion%ucrt\x64" set "LIB=%UniversalCRTSdkDir%Lib\%UCRTVersion%ucrt\x64;%LIB%"
+
+for /d %%d in ("%ProgramFiles(x86)%\Windows Kits\10\Include\*") do if not defined UCRT_INCLUDE if exist "%%~fd\ucrt\crtdefs.h" set "UCRT_INCLUDE=%%~fd\ucrt"
+for /d %%d in ("%ProgramFiles(x86)%\Windows Kits\10\Lib\*") do if not defined UCRT_LIB if exist "%%~fd\ucrt\x64\ucrt.lib" set "UCRT_LIB=%%~fd\ucrt\x64"
+if defined UCRT_INCLUDE set "INCLUDE=%UCRT_INCLUDE%;%INCLUDE%"
+if defined UCRT_LIB set "LIB=%UCRT_LIB%;%LIB%"
 
 where cl
 if errorlevel 1 exit /b 1
